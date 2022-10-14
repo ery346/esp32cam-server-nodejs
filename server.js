7e1938e30585c74dbac0e22913e4423e903e7ddb
david <david.s.n29@hotmail.com>
2022-10-13 20:53:15 -0600

    ¨1.0¨

---
+++ server.js
@@ -0,0 +1,53 @@
+const {initializeApp} = require('firebase/app');
+const { getDatabase, ref, onValue, set, update} = require('firebase/database');
+const express = require('express');
+const http = require('http');
+const port = process.env.PORT || 3000;
+const server = express();
+
+const SetInterval = require('set-interval');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCOoDU_3dejelgL0BUmaRteeTtFpt9xqpc",
+    authDomain: "esp8266-test1-86ada.firebaseapp.com",
+    databaseURL: "https://esp8266-test1-86ada-default-rtdb.firebaseio.com",
+    projectId: "esp8266-test1-86ada",
+    storageBucket: "esp8266-test1-86ada.appspot.com",
+    messagingSenderId: "1063224577587",
+    appId: "1:1063224577587:web:cc243e2b0ca407c90e9d46"
+  };
+
+
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+
+
+  readHeartPulses();
+  function readHeartPulses(){
+     const changeRef = ref(db, 'isAlive/' + 'vitalSigns/' );
+     onValue(changeRef, (snapshot) => {
+         if (snapshot) { // detecta el cambio 
+             console.log('status on');
+             SetInterval.clear('test')
+             SetInterval.start(ca, 12000, 'test')
+            function ca(){
+             console.log('status off');
+             const updateRef = ref(db, 'isAlive/' );
+             update(updateRef, {status: false})
+                SetInterval.clear('test')
+             }
+         }
+         });
+  }
+
+  setInterval(() => {
+    try {
+        http.get("https://neon-fortune-mapusaurus.glitch.me");
+    } catch (error) {
+        // next(error);
+    }
+}, 258000); // cada 4.30 minutos (258000)
+
+  server.listen(port, () => {
+    // console.log('Servidor corriendo');
+})
